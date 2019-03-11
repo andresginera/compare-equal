40f368063313e3ce18ca23c137676d7dc67387eb
Andres Giner Anton <andresgineranton@outlook.com>
2019-03-11 10:39:01 +0100

    changes on __init__.py file

--- compare_equal/__init__.py
+++ compare_equal/__init__.py
@@ -1,7 +1,6 @@
+name = "Compare-Equal"
 """
-Compare_equal package is composed of two main functions for comparison two molecules and checks if they are the same molecule. Both functions are inside of the main module :mod:`compare_equal.compare`. 
+Compare_equal package is composed of two main functions for comparison two molecules and checks if they are the same molecule.
 """
-
-name = "compare_equal"
-__all__ = ["compare"]
-from compare_equal import *
+__all__ = ["ComparePdb", "CompareMol"]
+from compare import *
