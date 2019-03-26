c5281a9438ca858f3fa40a09a0570b74796a86de
Andres Giner Anton <andresgineranton@outlook.com>
2019-03-26 15:43:08 +0100

    new tests

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-The subpackage tests consists of two modules: pdb_test and mol_test for checking both functions ComparePdb and CompareMol.
-"""
-
-__author__ = "Andres Giner Anton"
-__url__ = "https://github.com/andresginera/compare-equal"
-__title__ = "Compare Equal"
---
+++ tests/test_class.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+sys.path.insert(0, "/opt/miniconda2/lib/python2.7/site-packages/")
+
+from pychimera import patch_environ, enable_chimera
+
+patch_environ(nogui=True)
+# enable_chimera()
+
+
+from context import compare
+import chimera
+
+
+class Test_Compare(object):
+    def test_pdb(self):
+        assert compare.compare_pdb("tests/data/mol1", "tests/data/mol2")
+        assert not compare.compare_pdb("tests/data/mol1", "tests/data/mol3")
+        assert not compare.compare_pdb("tests/data/mol2", "tests/data/mol3")
+
+    def test_mol(self):
+        """Test fot the function compare_mol"""
+
+        path = os.path.dirname(os.path.abspath(__file__))
+        mol1 = chimera.openModels.open(os.path.join(path, "data/mol1.pdb"), type="PDB")[
+            0
+        ]
+        mol2 = chimera.openModels.open(os.path.join(path, "data/mol2.pdb"), type="PDB")[
+            0
+        ]
+        mol3 = chimera.openModels.open(os.path.join(path, "data/mol3.pdb"), type="PDB")[
+            0
+        ]
+
+        assert compare.compare_mol(mol1, mol2)
+        assert not compare.compare_mol(mol1, mol3)
+        assert not compare.compare_mol(mol2, mol3)
