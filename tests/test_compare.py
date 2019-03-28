dc5733c978d31d386845fcdc186d97bda9a0923f
Andres Giner Anton <andresgineranton@outlook.com>
2019-03-28 09:40:49 +0100

    added abs.path in test_mol

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -20,10 +20,15 @@ def test_pdb():
 def test_mol():
     """Test fot the function compare_mol"""
 
-    path = os.path.dirname(os.path.abspath(__file__))
-    mol_1 = chimera.openModels.open(os.path.join(path, "data/mol1.pdb"), type="PDB")[0]
-    mol_2 = chimera.openModels.open(os.path.join(path, "data/mol2.pdb"), type="PDB")[0]
-    mol_3 = chimera.openModels.open(os.path.join(path, "data/mol3.pdb"), type="PDB")[0]
+    mol_1 = chimera.openModels.open(os.path.abspath("tests/data/mol1.pdb"), type="PDB")[
+        0
+    ]
+    mol_2 = chimera.openModels.open(os.path.abspath("tests/data/mol2.pdb"), type="PDB")[
+        0
+    ]
+    mol_3 = chimera.openModels.open(os.path.abspath("tests/data/mol3.pdb"), type="PDB")[
+        0
+    ]
 
     assert compare.compare_mol(mol_1, mol_2)
     assert not compare.compare_mol(mol_1, mol_3)
