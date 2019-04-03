3135ef2440acf9b3ebbc4e27f295f045ed67e844
Andres Giner Anton <andresgineranton@outlook.com>
2019-04-03 17:37:50 +0200

    tests: remove unnecessary import

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pychimera import patch_environ, enable_chimera
-
-patch_environ()
-enable_chimera()
-
 import os
-import chimera
 
 TESTPATH = os.path.dirname(os.path.abspath(__file__))
 
